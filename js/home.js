003316fa98f6814c24983a53e44609f99ed04290
carolina silva <carolinasguzman@gmail.com>
2020-04-18 16:53:12 -0400

    feat:cambios

--- js/home.js
+++ js/home.js
@@ -1,5 +1,37 @@
-function crearTarea() {
+function crearTareaPanel() {
   console.log('****entre y cree tarea')
-  $("#crearTarea").val('Crear Tarea 1 para carolina')
+  var database = firebase.database();
+  var docRef = db.collection("paneles");
+  var userId = firebase.auth().currentUser.uid;
+  database.ref('/paneles').once('value').then(function(snapshot) {
+      console.log('snapshot', snapshot)
+  // ...
+  });
+  /*db.collection("paneles").add({
+    
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+  })
+  .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+      console.error("Error adding document: ", error);
+  });*/
+
+    $("#crearTareaPanel").val('Crear Tarea 1 para carolina')
+  }
+
+function volverDeAlerta() {
+  window.location.href = "./home.html"
+}
+
+function salirSesion() {
+  firebase.auth().signOut().then(function() {
+    window.location.href = "../index.html";
+  }).catch(function(error) {
+    // An error happened.
+  });
 }
 
