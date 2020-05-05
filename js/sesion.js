ffc8073f4dd50e01212f0544573c1eb941d5a915
carolina silva <carolinasguzman@gmail.com>
2020-05-04 20:31:53 -0400

    feat:validar

--- js/sesion.js
+++ js/sesion.js
@@ -1,7 +1,6 @@
+
 function iniciarSesion() {
-  console.log("hola");
   let email = $("#user").val();
-  console.log('¨¨¨¨****+email', email)
   let password = $("#inputPassword").val();
   firebase.auth().signInWithEmailAndPassword(email, password).then(function(result) {
     window.location.href = "./app/home.html"
@@ -20,11 +19,21 @@ function iniciarSesion() {
 function habilitarBoton() {
   let correo = $("#user").val();
   let password = $("#inputPassword").val();
-  let check = $("#exampleCheck1").val();
-  if (correo !== '' && password !== '' && check !== '') {
+  if (correo !== '' && password !== '' && ValidateEmail()) {
     $("#inicioSesion").removeClass("btn-secondary")
     $("#inicioSesion").removeAttr("disabled")
     $("#inicioSesion").addClass("btn-primary") 
     $("#inicioSesion").show()
   }
+}
+
+function ValidateEmail() 
+{
+ if (/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test($("#user").val()))
+  {
+    $("#validarCorreo").html('');
+    return (true)
+  }
+  $("#validarCorreo").html('   correo invalido!');
+  return (false)
 }
